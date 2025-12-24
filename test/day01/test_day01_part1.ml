fedf8d99329ac006607ff286c305cd343aba19f5
Shyiu <mightydragon334@gmail.com>
2025-12-24 18:55:44 -0500

    Finished day 1 part 1

---
+++ test/day01/test_day01_part1.ml
@@ -0,0 +1,104 @@
+open! Core
+open! Stdio
+open! Hardcaml
+open! Hardcaml_waveterm
+open! Hardcaml_test_harness
+
+module Part1 = Day01.Day01_part1
+module Harness = Cyclesim_harness.Make (Part1.I) (Part1.O)
+
+let ( <--. ) = Bits.( <--. )
+
+let simple_testbench (sim : Harness.Sim.t) =
+  let inputs = Cyclesim.inputs sim in
+  let outputs = Cyclesim.outputs sim in
+  let cycle ?n () = Cyclesim.cycle ?n sim in
+  (* Helper function for inputting one value *)
+  let feed_input n =
+    inputs.data_in <--. n;
+    inputs.data_in_valid := Bits.vdd;
+    cycle ();
+    inputs.data_in_valid := Bits.gnd;
+    cycle ()
+  in
+  (* Reset the design *)
+  inputs.clear := Bits.vdd;
+  cycle ();
+  inputs.clear := Bits.gnd;
+  cycle ();
+  (* Pulse the start signal *)
+  inputs.start := Bits.vdd;
+  cycle ();
+  inputs.start := Bits.gnd;
+  
+  let lines = Stdio.In_channel.read_lines "day01.txt" in
+  List.iter lines ~f:(fun line ->
+    let direction = String.get line 0 in
+    let number = int_of_string (String.sub line ~pos:1 ~len:(String.length line - 1)) in
+    feed_input (if Char.equal direction 'L' then (-1 * number) else number)
+  );
+  inputs.finish := Bits.vdd;
+  cycle ();
+  inputs.finish := Bits.gnd;
+  cycle ();
+  (* Wait for result to become valid *)
+  while not (Bits.to_bool !(outputs.output.valid)) do
+    cycle ()
+  done;
+  let output = Bits.to_unsigned_int !(outputs.output.value) in
+  print_s [%message "Result" (output : int)];
+  (* Show in the waveform that [valid] stays high. *)
+  cycle ~n:2 ()
+;;
+
+(* The [waves_config] argument to [Harness.run] determines where and how to save waveforms
+   for viewing later with a waveform viewer. The commented examples below show how to save
+   a waveterm file or a VCD file. *)
+(* let waves_config = Waves_config.no_waves *)
+
+(* let waves_config = *)
+(*   Waves_config.to_directory "/tmp/" *)
+(*   |> Waves_config.as_wavefile_format ~format:Hardcamlwaveform *)
+(* ;; *)
+
+let waves_config =
+  Waves_config.to_directory "/tmp/"
+  |> Waves_config.as_wavefile_format ~format:Vcd
+;;
+
+let%expect_test "Simple test, optionally saving waveforms to disk" =
+  Harness.run_advanced ~waves_config ~create:Part1.hierarchical simple_testbench;
+  [%expect {|
+    (Result (output 1100))
+    Saved waves to /tmp/test_day01_part1_ml_Simple_test__optionally_saving_waveforms_to_disk.vcd
+    |}]
+;;
+
+let%expect_test "Simple test with printing waveforms directly" =
+  (* For simple tests, we can print the waveforms directly in an expect-test (and use the
+     command [dune promote] to update it after the tests run). This is useful for quickly
+     visualizing or documenting a simple circuit, but limits the amount of data that can
+     be shown. *)
+  let display_rules =
+    [ Display_rule.port_name_matches
+        ~wave_format:(Bit_or Unsigned_int)
+        (Re.Glob.glob "Part1*" |> Re.compile)
+    ]
+  in
+  Harness.run_advanced
+    ~create:Part1.hierarchical
+    ~trace:`All_named
+    ~print_waves_after_test:(fun waves ->
+      Waveform.print
+        ~display_rules
+          (* [display_rules] is optional, if not specified, it will print all named
+             signals in the design. *)
+        ~signals_width:30
+        ~display_width:92
+        ~wave_width:1
+        (* [wave_width] configures how many chars wide each clock cycle is *)
+        waves)
+    simple_testbench;
+  [%expect
+    {| (Result (output 1100)) |}]
+;;
